a8f2af77bb7032ad5048dd40edba1311580b6835
InJung Chung <mu29gl@gmail.com>
2015-09-22 00:06:57 +0900

    Facebook bot for upload posts

---
+++ facebook_bot.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import facebook
+from settings import *
+
+class FacebookBot:
+
+    def __init__(self):
+        config = {
+            'page_id'      : PAGE_ID,
+            'access_token' : ACCESS_TOKEN
+        }
+        self.api = self.get_api(config)
+
+    def get_api(self, config):
+        graph = facebook.GraphAPI(config['access_token'])
+        response = graph.get_object('me/accounts')
+        page_access_token = None
+        for page in response['data']:
+            if page['id'] == config['page_id']:
+                page_access_token = page['access_token']
+        graph = facebook.GraphAPI(page_access_token)
+        return graph
+
+    def post(self, message, file):
+        api.put_wall_post(message)
