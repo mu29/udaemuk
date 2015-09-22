3c4ef85cc8ad25801b8473665ccc9403ad737786
InJung Chung <mu29gl@gmail.com>
2015-09-22 20:01:25 +0900

    posting reports

--- report.py
+++ report.py
@@ -5,6 +5,7 @@ from flask import *
 from werkzeug import secure_filename
 from server import app
 from settings import ALLOWED_EXTENSIONS
+from facebook_bot import fbBot
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -13,16 +14,32 @@ def allowed_file(filename):
 def login():
     # POST : 전송
     if request.method == 'POST':
-        print request.form
         name = request.form['name']
         photo = request.files['file']
         quality = request.form['quality']
         cost = request.form['cost']
         comment = request.form['comment']
 
+        message = ''
+        message += name + '\n'
+        message += u'퀄리티 : '
+        for i in range(0, int(quality)):
+            message += u'★'
+        message += '\n'
+        message += u'가성비 : '
+        for i in range(0, int(cost)):
+            message += u'★'
+        message += '\n'
+        message += u'코멘트 : ' + comment
+        message = message.encode('utf-8')
+
+        filename = None
         if photo and allowed_file(photo.filename):
             filename = secure_filename(photo.filename)
             photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        fbBot.post(message, filename)
+
         return ""
     elif request.method == 'GET':
         # GET : 입력폼 보이기
